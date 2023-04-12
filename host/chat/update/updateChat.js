afc135917b3e8ecdac47104a530c155296a5a1a8
Will Tesler <willtesler@gmail.com>
2023-04-11 20:48:38 -0500

    limit chat to 20

--- host/chat/update/updateChat.js
+++ host/chat/update/updateChat.js
@@ -1,5 +1,16 @@
+/**
+ *
+ * @param {any[]} chats
+ * @param {AbortSignal} abortSignal
+ * @param {ServerResponse} res
+ * @returns {Promise<void>}
+ */
 module.exports = async function (chats, abortSignal, res) {
   const openAiClient = require("../../openai/client/OpenAiClient");
+
+  if (chats.length > 20) {
+    chats.slice(0, 20)
+  }
 
   for await (const tokens of openAiClient.updateChatAsync(chats, abortSignal)) {
     res.write(tokens)
