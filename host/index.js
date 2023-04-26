67e8fb333600b6345cc40a5bddc63b8c2cf8d622
Will Tesler <willtesler@gmail.com>
2023-04-26 18:52:23 -0500

    Tweak

--- host/index.js
+++ host/index.js
@@ -5,10 +5,12 @@
   const cors = require("cors");
   const {discoverEndpoints} = require("cranny");
 
+  const isDevelopment = process.env.NODE_ENV === 'development';
+
   const app = express();
 
   // app.use(express.urlencoded({limit: '20mb', extended: true}));
-  app.use(express.json({limit: '1mb'}));
+  app.use(express.json({limit: '100kb'}));
 
   app.use(cors());
 
@@ -20,7 +22,10 @@
       const name = endpoint.name;
       const func = endpoint.obj;
       app[type](`/${name}`, func);
-      console.log(`${name} | ${type.toUpperCase()}`);
+
+      if (isDevelopment) {
+        console.log(`${name} | ${type.toUpperCase()}`);
+      }
     }
   };
 
@@ -28,7 +33,7 @@
 
   const port = process.env.PORT || 8080;
 
-  if (process.env.NODE_ENV === 'development') {
+  if (isDevelopment) {
     await hostDevelopment(app, port);
   } else {
     hostProduction(app, port);
@@ -42,7 +47,6 @@
 async function hostDevelopment(app, port) {
   const os = require('os');
   const https = require('https');
-
 
   let host = 'localhost';
   const networkInterfaces = os.networkInterfaces();
@@ -62,6 +66,11 @@ async function hostDevelopment(app, port) {
   }
 
   const server = https.createServer({key: process.env.DEV_HTTPS_KEY, cert: process.env.DEV_HTTPS_CERT}, app);
+
+  app.use(function(req, res, next) {
+    console.log(`${req.method} ${req.url}`);
+    next();
+  });
 
   server.listen(port, host, () => {
     console.log(`Development server listening on https://${host}:${port}`);
